e8e360c0cee53a6aa3c8e3abce9772113feb0ed5
Agus Heryanto <agusheryantodev@gmail.com>
2024-10-20 22:57:36 +0700

    feat(bulk-insert): adding bulk insert feature

--- index.js
+++ index.js
@@ -23,23 +23,44 @@ async function main() {
     case "check-db-connection":
       await checkConnection();
       break;
-    // TODO: Buat logic fungsionalitas yg belum tersedia di bawah
+    case "bulk-insert":
+      await bulkInsert();
+      break;
     default:
       throw Error("command not found");
   }
 
   await mongoose.disconnect();
 
-  async function checkConnection() {
-    console.log("check db connection started...");
-    try {
-      await mongoose.connection.db.admin().ping();
-      console.log("MongoDB connection is successful!");
-    } catch (err) {
-      console.error("MongoDB connection failed:", err);
+  async function bulkInsert() {
+    console.log("bulk insert started...");
+    const bulk = Model.collection.initializeUnorderedBulkOp();
+    dataSeeder.forEach((item) => {
+      bulk.insert(item);
+    });
+    const result = await bulk.execute();
+    console.log("bulk insert result:", result);
+
+    if (result) {
+      console.log("bulk insert success");
+    } else {
+      console.log("bulk insert failed");
     }
-    console.log("check db connection ended...");
+    console.log("bulk insert ended...");
   }
+
+  return;
+}
+
+async function checkConnection() {
+  console.log("check db connection started...");
+  try {
+    await mongoose.connection.db.admin().ping();
+    console.log("MongoDB connection is successful!");
+  } catch (err) {
+    console.error("MongoDB connection failed:", err);
+  }
+  console.log("check db connection ended...");
 }
 
 main();
